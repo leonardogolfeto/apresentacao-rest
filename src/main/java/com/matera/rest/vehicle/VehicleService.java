90d3919b348d98594eeab49a9ef9c2329ef3a662
Gabriel Oswaldo <gabriel.oswaldo99@gmail.com>
2020-12-24 11:18:59 -0300

    Revert "Fix repository annotation"
    This reverts commit 3b76c5a57955d5ab5e08ac807502cdf3b1996f63.

--- src/main/java/com/matera/rest/vehicle/VehicleService.java
+++ src/main/java/com/matera/rest/vehicle/VehicleService.java
@@ -2,9 +2,7 @@ package com.matera.rest.vehicle;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
-import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,19 +12,15 @@ public class VehicleService {
     private final VehicleRepository vehicleRepository;
 
     @Autowired
-    public VehicleService(final VehicleRepository vehicleRepository) {
+    public VehicleService(VehicleRepository vehicleRepository) {
         this.vehicleRepository = vehicleRepository;
     }
 
-    public Optional<Vehicle> getVehicleByLicensePlate(final String licensePlate) {
+    public Optional<Vehicle> getVehicleByLicensePlate(String licensePlate) {
         return this.vehicleRepository.findById(licensePlate);
     }
 
     public List<Vehicle> getVehicles() {
         return vehicleRepository.findAll();
-    }
-
-    public Vehicle insertVehicle (VehicleDTO vehicle) {
-        return vehicleRepository.save(new Vehicle(vehicle));
     }
 }
